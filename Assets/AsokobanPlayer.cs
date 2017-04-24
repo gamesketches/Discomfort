750a964ab24fde8523374e6d97c4f53a83e297d4
gamesketches <sam.von.ehren@gmail.com>
2017-04-24 00:29:32 -0400

    Most final work for touch typist, art assets for Asokoban

---
+++ Assets/AsokobanPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsokobanPlayer : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+	Animator animationController;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		animationController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis("Vertical");
+
+		rigidBody.MovePosition(rigidBody.position + (new Vector2(hori, vert) * Time.deltaTime));
+		animationController.SetInteger("XMovement", (int)hori);
+		animationController.SetInteger("YMovement", (int)vert);
+	}
+}
