0e9ba4481b5bdb164849c1f77f19841cd9eb1ce4
gamesketches <sam.von.ehren@gmail.com>
2017-04-24 03:11:02 -0400

    Adjustments and tuning to asokoban player

--- Assets/AsokobanPlayer.cs
+++ Assets/AsokobanPlayer.cs
@@ -6,10 +6,14 @@ public class AsokobanPlayer : MonoBehaviour {
 
 	Rigidbody2D rigidBody;
 	Animator animationController;
+	SpriteRenderer renderer;
+	BoxCollider2D collider;
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D>();
 		animationController = GetComponent<Animator>();
+		renderer = GetComponent<SpriteRenderer>();
+		collider = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
@@ -20,5 +24,17 @@ public class AsokobanPlayer : MonoBehaviour {
 		rigidBody.MovePosition(rigidBody.position + (new Vector2(hori, vert) * Time.deltaTime));
 		animationController.SetInteger("XMovement", (int)hori);
 		animationController.SetInteger("YMovement", (int)vert);
+		if(hori > 0) {
+			renderer.flipX = true;
+			Vector2 baseOffset = collider.offset;
+			baseOffset.x = Mathf.Abs(baseOffset.x);
+			collider.offset = baseOffset;
+		}
+		else if(hori < 0) {
+			renderer.flipX = false;
+			Vector2 baseOffset = collider.offset;
+			baseOffset.x = Mathf.Abs(baseOffset.x) * -1;
+			collider.offset = baseOffset;
+		}
 	}
 }
