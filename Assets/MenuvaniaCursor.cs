f1642747aba6c18054d4d473317fdb056ef1793c
gamesketches <sam.von.ehren@gmail.com>
2017-05-04 00:14:16 -0400

    Starting Menuvania

---
+++ Assets/MenuvaniaCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuvaniaCursor : MonoBehaviour {
+
+	public GameObject[] drawers;
+	int focusDepth;
+	Transform focusedItem;
+	// Use this for initialization
+	void Start () {
+		drawers = GameObject.FindGameObjectsWithTag("Drawer");
+		focusedItem = drawers[0].transform;
+		RepositionCursor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			focusedItem.gameObject.GetComponent<DrawerBehaviour>().ToggleDrawer();
+		}
+		RepositionCursor();
+	}
+
+	void RepositionCursor() {
+		transform.position = focusedItem.position - new Vector3(0, 10, 0);
+	}
+}
