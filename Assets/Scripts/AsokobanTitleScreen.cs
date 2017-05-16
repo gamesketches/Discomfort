47399a522ad0ef17e0827385c3edac0a3427fcd0
gamesketches <sam.von.ehren@gmail.com>
2017-05-15 21:55:18 -0400

    Making title screen for asokoban

---
+++ Assets/Scripts/AsokobanTitleScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AsokobanTitleScreen : MonoBehaviour {
+
+	public float rotationAmount;
+	public float oscillationTime;
+	AnimationCurve rotationCurve;
+	public Image[] UIImages;
+	public Text UIText;
+	// Use this for initialization
+	void Start () {
+		rotationCurve = new AnimationCurve();
+		rotationCurve.AddKey(0, 0);
+		rotationCurve.AddKey(oscillationTime, rotationAmount);
+		rotationCurve.AddKey(oscillationTime * 2, -rotationAmount);
+		rotationCurve.AddKey(oscillationTime * 3, 0);
+		rotationCurve.postWrapMode = WrapMode.Loop;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0, 0, rotationCurve.Evaluate(Time.time));
+		if(Input.anyKeyDown) {
+			foreach(Image img in UIImages) {
+				img.CrossFadeAlpha(0.0f, 0.5f, false);
+			}
+			UIText.CrossFadeAlpha(0.0f, 0.5f, false);
+		}
+	}
+
+}
