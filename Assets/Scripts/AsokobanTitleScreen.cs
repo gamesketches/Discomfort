49a9e568cf335eef02b8db5f32f9c0ee32bbcf10
gamesketches <sam.von.ehren@gmail.com>
2017-05-15 23:26:03 -0400

    Need to fix the monkey pivot but otherwise Asokoban titlescreen is in

--- Assets/Scripts/AsokobanTitleScreen.cs
+++ Assets/Scripts/AsokobanTitleScreen.cs
@@ -8,10 +8,19 @@ public class AsokobanTitleScreen : MonoBehaviour {
 	public float rotationAmount;
 	public float oscillationTime;
 	AnimationCurve rotationCurve;
+	public float zoomInTime = 1;
 	public Image[] UIImages;
 	public Text UIText;
+	Vector3 cameraTarget;
+	float cameraSize;
+	bool started;
 	// Use this for initialization
 	void Start () {
+		started = false;
+		cameraTarget = Camera.main.transform.position;
+		cameraSize = Camera.main.orthographicSize;
+		Camera.main.transform.position = new Vector3(0, 0, -10);
+		Camera.main.orthographicSize = 28;
 		rotationCurve = new AnimationCurve();
 		rotationCurve.AddKey(0, 0);
 		rotationCurve.AddKey(oscillationTime, rotationAmount);
@@ -22,13 +31,24 @@ public class AsokobanTitleScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.Euler(0, 0, rotationCurve.Evaluate(Time.time));
-		if(Input.anyKeyDown) {
-			foreach(Image img in UIImages) {
-				img.CrossFadeAlpha(0.0f, 0.5f, false);
+		if(!started) {
+			transform.rotation = Quaternion.Euler(0, 0, rotationCurve.Evaluate(Time.time));
+			if(Input.anyKeyDown) {
+				foreach(Image img in UIImages) {
+					img.CrossFadeAlpha(0.0f, 0.5f, false);
+				}
+				UIText.CrossFadeAlpha(0.0f, 0.5f, false);
+				StartCoroutine(Onboarding());
+				started = true;
 			}
-			UIText.CrossFadeAlpha(0.0f, 0.5f, false);
 		}
 	}
 
+	IEnumerator Onboarding() {
+		for(float t = 0; t <= zoomInTime; t += Time.deltaTime) {
+			Camera.main.transform.position = Vector3.Lerp(new Vector3(0, 0, -10), cameraTarget, t / zoomInTime);
+			Camera.main.orthographicSize = Mathf.SmoothStep(28, cameraSize, t / zoomInTime);
+			yield return null;
+		}
+	}
 }
